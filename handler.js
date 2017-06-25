53afc480cdcca04fb81e53ead1f6f6321631fc4d
patmood <patrick.n.moody@gmail.com>
2017-06-25 14:09:31 -0700

    add summary to content

--- handler.js
+++ handler.js
@@ -11,30 +11,45 @@ To test this locally, run:
 
 const fetch = require('node-fetch')
 const extract = require('unfluff')
+const summary = require('node-summary')
+
+function summarizeText(title, content) {
+  return new Promise((resolve, reject) => {
+    summary.summarize(title, content, (err, summary) => {
+      if (err) reject(err)
+      resolve(summary)
+    })
+  })
+}
 
 module.exports.getArticle = (event, context, callback) => {
   if (!event.pathParameters.id) {
     return callback(null, { statusCode: 400 });
   }
 
-  let url = null
+  let responseBody = {}
 
   fetch(`https://www.reddit.com/${event.pathParameters.id}.json`)
     .then(res => res.json())
     .then(json => {
       const post = json[0].data.children
-      url = post[0].data.url
+      const url = post[0].data.url
       return fetch(url)
     })
     .then(res => res.text())
     .then(text => extract(text))
     .then(data => {
+      responseBody = Object.assign({}, data)
+      return summarizeText(data.title, data.text)
+    })
+    .then(summary => {
+      responseBody.summary = summary
       const response = {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*', // Required for CORS support to work
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(responseBody),
       }
       callback(null, response)
     })
