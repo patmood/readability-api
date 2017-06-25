9f7fc39ce113ef0e14714fde883cb46824bc34e2
patmood <patrick.n.moody@gmail.com>
2017-06-25 13:38:48 -0700

    return readable data

--- handler.js
+++ handler.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const fetch = require('node-fetch')
+const read = require('node-readability')
+
+
 /*
 
 Endpoint to return readable content for given reddit article id.
@@ -9,28 +13,47 @@ To test this locally, run:
 
 */
 
+function getReadablePage(url) {
+  return new Promise((resolve, reject) => {
+    read(url, (err, data) => {
+      if(err) reject(err);
+      resolve(data)
+    })
+  })
+}
+
 module.exports.getArticle = (event, context, callback) => {
   if (!event.pathParameters.id) {
     return callback(null, { statusCode: 400 });
   }
 
+  let url = null
+
   fetch(`https://www.reddit.com/${event.pathParameters.id}.json`)
     .then(res => res.json())
     .then(json => {
       const post = json[0].data.children
-      const url = post[0].data.url
-      console.log(url)
+      url = post[0].data.url
+      return getReadablePage(url)
+    })
+    .then(article => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        },
+        body: JSON.stringify({
+          title: article.title,
+          content: article.content,
+          url: url,
+          redditId: event.pathParameters.id,
+        }),
+      }
+      callback(null, response)
+    })
+    .catch(err => {
+      console.log(err)
     })
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-    }),
-  };
 
-  callback(null, response);
-};
+}
